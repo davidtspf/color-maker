95dd50875be17c2025d8db2aa09855c11f8fdff9
David Tannenbaum <davidtspf@users.noreply.github.com>
2020-05-09 13:22:05 -0700

    Made color maker sliders

---
+++ color maker sliders/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  color maker sliders
+//
+//  Created by DT on 5/5/20.
+//  Copyright © 2020 DT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func redChange(_ sender: Any) {
+       changeColorView()
+    }
+    
+    @IBAction func greenChange(_ sender: Any) {
+         changeColorView()
+    }
+    
+    @IBAction func blueChange(_ sender: Any) {
+         changeColorView()
+    }
+    
+    @IBAction func changeColorView() {
+           colorView.backgroundColor = UIColor(red: CGFloat(self.redControl.value)/255, green: CGFloat(self.greenControl.value)/255, blue: CGFloat(self.blueControl.value)/255, alpha: 1)
+       }
+
+}
---
+++ color maker slidersTests/color_maker_slidersTests.swift
@@ -0,0 +1,34 @@
+//
+//  color_maker_slidersTests.swift
+//  color maker slidersTests
+//
+//  Created by DT on 5/5/20.
+//  Copyright © 2020 DT. All rights reserved.
+//
+
+import XCTest
+@testable import color_maker_sliders
+
+class color_maker_slidersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
